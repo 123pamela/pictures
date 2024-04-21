bac1489cbbae9742d3652e5eb7571579201dec0b
123pamela <delacruzgaliciapamela@cbtis116.edu.mx>
2024-04-20 17:23:26 -0700

    modifique el archivo api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const searchImage = async(term) => {
-    const url = 'https://api.unsplash.com/search/photos/?client_id='
+    const url = 'https://api.unsplash.com/search/photos/?client_id=w5--aaF4N6t4wmrWh3XfDzV9zI2uQst509DgHGUimJ0'
     const response = await axios .get(url, {
 params: {
     query: term
